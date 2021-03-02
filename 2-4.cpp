bad88fc08a7302eab2271043c5c87a208751e516
elee91 <emlee54@gmail.com>
2021-03-02 19:06:17 +0000

    Classwork 2-4

---
+++ 2-4.cpp
@@ -0,0 +1,20 @@
+// Assignment 2-4
+// Print Alphabets with ASCII code
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i = 'A';
+	int j = 'Z';
+
+	while ( i <= j )
+	{
+		cout << static_cast<char>(i) << " ";
+		i++;
+		if ( i % 5 == 0 )
+			cout << "\n";
+	}
+	return 0;
+}
