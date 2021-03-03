18d2e3eaf208d3f03392a3b77660fdf4df56a510
elee91 <77417178+elee91@users.noreply.github.com>
2021-03-02 16:05:52 -0800

    Create 2-5.cpp

---
+++ 2-5.cpp
@@ -0,0 +1,23 @@
+// Assignment 2-5
+// Find list of prime numbers in given range
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int begin, end;
+    int i, j;
+
+    do {
+        cout << "Enter two positive integers in accending order : ";
+        cin >> begin >> end;
+    } while ( end < begin || begin < 1 || end < 1 );
+
+    cout << "The following primes are in range [" << begin << ", " << end << "]: \n";
+    for ( i = begin; i <= end; i++ ) {
+        for ( j = 2; j < (i/j); j++ ) 
+            if ( !(i % j) ) break;
+        if ( j > (i/j) ) cout << i << " ";
+    }
+    return 0;
+} 
