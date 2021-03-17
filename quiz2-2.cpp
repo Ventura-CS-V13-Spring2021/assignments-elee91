fbd32157489a0bd6897e7755d9a6c96922b38a47
elee91 <emlee54@gmail.com>
2021-03-17 19:15:16 +0000

    commit

--- quiz2-2.cpp
+++ quiz2-2.cpp
@@ -4,10 +4,16 @@ using namespace std;
 
 int main()
 {
-	int randN;
+	int randN, max1, max2;
 	srand(time(0);
 	for ( int i; i < 5; i++ ) {
-		
+		randN = rand() % 100;
+		if ( i == 0 )
+			max1 = max2 = randN;
+		do {
+			max1 = randN;
+		} while ( max1 > randN );
+			
 	}
 
 	// Question 1
