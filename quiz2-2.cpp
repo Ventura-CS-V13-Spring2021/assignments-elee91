195181bce157e3235f18ba9b5806eb74a6f0b703
elee91 <emlee54@gmail.com>
2021-03-17 21:33:21 +0000

    commit, started problem 3

--- quiz2-2.cpp
+++ quiz2-2.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main()
 {
+// Problem 1	
 	int randN, max1, max2;
 	int i, j = 0;
 
@@ -18,15 +19,37 @@ int main()
 				max1 = randN;
 		}
 		do {
-			if ( max2 < randN && randN < max1 )
-				max2 = randN;
+			if ( randN >= max1 || j >= i )
 				break;
-		} while ( j < i );
+			else {
+				max2 = randN;
+				j++;
+			}
+		} while ( max2 < randN );
 
 		cout << randN << " ";
 	}
-	
-	cout << "\nIn sequence of the random numbers generated," << endl;
+//	cout << "\nIn sequence of the random numbers generated," << endl;
 	cout << max1 << " is greater than the previous number " << max2 << endl;
+*/
+
+
+// Problem 3
+	int num1, num2, num3;
+	
+	do {}
+		cout << "Input 3 integers between [0-99] : ";
+		cin >> num1 >> num2 >> num3;
+	while ( num1 < 0 || num1 > 99 || num1 < 0 || num1 > 99 || num1 < 0 || num1 > 99);
+
+	if ( num1 == num2 && num1 == num3 )
+		cout << "All numbers are the same." << endl;
+	else if ( num1 == num2 || num1 == num3 || num2 == num3 )
+		cout << "There are two duplicated numbers." << endl;
+	else
+		cout << "There is no duplicated number." << endl;
+	return 0;
+
+
 }
 
