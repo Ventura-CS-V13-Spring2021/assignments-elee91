c12a7d914a6dc7e274c2532d339146888e6ce7b9
elee91 <emlee54@gmail.com>
2021-03-17 19:27:35 +0000

    commit

--- quiz2-2.cpp
+++ quiz2-2.cpp
@@ -5,18 +5,20 @@ using namespace std;
 int main()
 {
 	int randN, max1, max2;
-	srand(time(0);
-	for ( int i; i < 5; i++ ) {
+	srand(time(0));
+	for ( int i = 0; i < 5; i++ ) {
 		randN = rand() % 100;
 		if ( i == 0 )
 			max1 = max2 = randN;
 		do {
 			max1 = randN;
-		} while ( max1 > randN );
-			
+		} while ( max1 < randN );
+		do {
+			max2 = randN;
+		} while ( max2 < max1 );
+		cout << randN << " ";
 	}
-
-	// Question 1
-	
+	cout << "\nIn sequence of the random numbers generated," << endl;
+	cout << max1 << " is greater than the previous number " << max2 << endl;
 }
 
