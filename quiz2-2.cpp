dfd3cb5b6739ae10f43df932494c54a44d653c81
elee91 <emlee54@gmail.com>
2021-03-18 00:25:52 +0000

    commit

--- quiz2-2.cpp
+++ quiz2-2.cpp
@@ -10,11 +10,11 @@ int main()
 
 // Problem 1	
 	int randN, max1, max2;
-	int p;
-	int i, j;
+	int i, j = 0, p;
 
+	cout << "In sequence of the random numbers generated," << endl;
 	srand(time(0));
-	for ( i = 0; i < 5; i++ ) {
+	for ( i = 0; i < 10; i++ ) {
 		randN = rand() % 100;
 		
 		if ( i == 0 )
@@ -22,19 +22,17 @@ int main()
 		else {
 			if ( max1 < randN ) {
 				max1 = randN;
-				p = i; // placeholder for position of max value
+				p = i;
 			}
 		}
-		cout << randN << " ";
-		for ( j = 0; j < p; j++ ) {
-			if ( max2 < randN && randN < max1 )
-				max2 = randN;
-				break;
+ 		while ( max2 < randN && j++ < p ) {
+			max2 = randN;
+			if ( j == p ) break;
 		}
+		cout << randN << " ";
 	}
-	cout << "\nIn sequence of the random numbers generated," << endl;
 	cout << endl;
-	cout << max1 << " is greater than the previous number " << max2 << endl;
+	cout << max1 << " is greater than " << max2 << endl;
 
 
 /*
