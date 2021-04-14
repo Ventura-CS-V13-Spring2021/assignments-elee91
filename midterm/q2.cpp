9d80a714c5d5e4505e8211f6241964226935c8d5
elee91 <>
2021-04-14 20:32:16 +0000

    save

--- midterm/q2.cpp
+++ midterm/q2.cpp
@@ -5,13 +5,13 @@
 #include <ctime>
 using namespace std;
 
-int getRdnum (void);
+int getRdnum(void);
 int isGreater(int);
 
 int main() {
     ofstream ofs;
     ofs.open("numbers.txt");
-	if ( ofs.fail()) {
+	if (ofs.fail()) {
 		cerr << "File open error\n.";
 		exit(0);
 	}
@@ -20,4 +20,22 @@ int main() {
 	}
 
     ofs.close();
+}
+
+int getRdnum(void) {
+	int N;
+	
+	srand(time(0));
+	N = (rand() % 50) + 1;
+	return N;
+}
+
+int isGreater(int n) {
+	int static previous = 0;
+	
+	if(n > previous)
+		previous = n;
+		return 1;
+	if(n < previous)
+		return 0;
 }
