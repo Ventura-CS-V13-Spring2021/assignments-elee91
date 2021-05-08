7aa04e2bc7c74df4d60b66940957208b10cead08
elee91 <>
2021-05-08 22:20:52 +0000

    commit

--- quiz7/coordinate.cpp
+++ quiz7/coordinate.cpp
@@ -1,26 +1,27 @@
 #include "coordinate.hpp"
+#include <iostream>
+using namespace std;
 
-Coordinate::Coordinate()
+Coordinate::Coordinate() : x(0), y(0)
 {
-
 }
-Coordinate::Coordinate(double xval, double yval)
+Coordinate::Coordinate(double xval, double yval) : x(xval), y(yval)
 {
-	
 }
 int Coordinate::getX() const
 {
-	
+	return x;
 }
 int Coordinate::getY() const
 {
-	
+	return y;
 }
 void Coordinate::setXY(double xval, double yval)
 {
-	
+	x = xval;
+	y = yval;
 }
 void Coordinate::printXY() const
 {
-	
+	cout << "( " << x << ", " << y << ")";
 }
--- quiz7/rectangle.cpp
+++ quiz7/rectangle.cpp
@@ -1,24 +1,24 @@
 #include "rectangle.hpp"
+#include <iostream>
+using namespace std;
 
-Rectangle::Rectangle()
+Rectangle::Rectangle() : lb(0.0), rt(0.0)
 {
-	
 }
-Rectangle::Rectangle(Coordinate lbval, Coordinate rtval)
+Rectangle::Rectangle(Coordinate lbval, Coordinate rtval) : lb(lbval), rt(rtval)
 {
-	
 }
 Coordinate Rectangle::getLB() const
 {
-	
+	return lb;
 }
 Coordinate Rectangle::getRT() const
 {
-	
+	return rt;
 }
 double Rectangle::getArea()
 {
-	
+	return 
 }
 Coordinate Rectangle::getCenter()
 {
