2c7406474dc3e9ba79ff2a5d54178d9e15ce1cd1
elee91 <>
2021-05-08 22:03:04 +0000

    quiz 7 begin

---
+++ quiz7/main.cpp
@@ -0,0 +1,17 @@
+// Ch07Q Class with class=type member variable
+#include "rectangle.hpp"
+#include "coordinate.hpp"
+
+int main()
+{
+    Coordinate lbval(0.0, 0.0);
+    Coordinate rtval(10.0, 10.0);
+    Rectangle r1(lbval, rtval);
+
+    r1.printRectangle();
+
+    lbval.setXY(-20.0, -20.0);
+    rtval.setXY(20.0, 20.0);
+    r1.setLBRT(lbval, rtval);
+    r1.printRectangle();
+}
