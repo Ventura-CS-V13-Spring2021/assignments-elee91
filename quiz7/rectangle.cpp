b44f2cb5f66a642e1e2466f27b6b00a2c324e324
elee91 <>
2021-05-08 22:31:14 +0000

    center formula

--- quiz7/rectangle.cpp
+++ quiz7/rectangle.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-Rectangle::Rectangle() : lb(0.0), rt(0.0)
+Rectangle::Rectangle() : lb(0.0, 0.0), rt(0.0, 0.0)
 {
 }
 Rectangle::Rectangle(Coordinate lbval, Coordinate rtval) : lb(lbval), rt(rtval)
@@ -18,11 +18,14 @@ Coordinate Rectangle::getRT() const
 }
 double Rectangle::getArea()
 {
-	return 
+	return (rt.getX() - lb.getX()) * (rt.getY() - lb.getY());
 }
 Coordinate Rectangle::getCenter()
 {
-	
+	double centX, centY;
+	centX = lb.getX() + (rt.getX()-lb.getX()) / 2;
+	centY = lb.getY() + (rt.getY()-lb.getY()) / 2;
+	Rectangle.setXY(centX, centY);
 }
 void Rectangle::setLBRT(Coordinate lbval, Coordinate rtval)
 {
