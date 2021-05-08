0b7fd13b5bec7dcc950a22eae29bf455dd200a59
elee91 <>
2021-05-08 22:10:36 +0000

    outline

---
+++ quiz7/coordinate.cpp
@@ -0,0 +1,26 @@
+#include "coordinate.hpp"
+
+Coordinate::Coordinate()
+{
+
+}
+Coordinate::Coordinate(double xval, double yval)
+{
+	
+}
+int Coordinate::getX() const
+{
+	
+}
+int Coordinate::getY() const
+{
+	
+}
+void Coordinate::setXY(double xval, double yval)
+{
+	
+}
+void Coordinate::printXY() const
+{
+	
+}
---
+++ quiz7/rectangle.cpp
@@ -0,0 +1,34 @@
+#include "rectangle.hpp"
+
+Rectangle::Rectangle()
+{
+	
+}
+Rectangle::Rectangle(Coordinate lbval, Coordinate rtval)
+{
+	
+}
+Coordinate Rectangle::getLB() const
+{
+	
+}
+Coordinate Rectangle::getRT() const
+{
+	
+}
+double Rectangle::getArea()
+{
+	
+}
+Coordinate Rectangle::getCenter()
+{
+	
+}
+void Rectangle::setLBRT(Coordinate lbval, Coordinate rtval)
+{
+	
+}
+void Rectangle::printRectangle() const
+{
+	
+}
