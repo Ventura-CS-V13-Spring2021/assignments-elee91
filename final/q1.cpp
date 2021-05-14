f617009b1c711432f221084d1eea3c5b63f6d304
elee91 <>
2021-05-14 20:49:56 +0000

    start final

---
+++ final/q1.cpp
@@ -0,0 +1,23 @@
+// You are given an array of integers that has the N positive integer elements 
+// with increasing sorted order. 
+// Find the element that has the greatest number of divisible elements
+#include <iostream>
+using namespace std;
+
+int getNumDiv(int, int)
+
+int main() 
+{
+	const int N = 6;
+	int arr[N] = {1, 2, 4, 6, 10, 24};
+
+	getNumDiv(arr, N);
+}
+
+int getNumDiv(int arr, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+
+	}
+}
