8a7ebc1d1cc5cc97f67aeddf37ece6c84f0c65fd
elee91 <>
2021-05-14 21:52:14 +0000

    commit

--- final/q2.cpp
+++ final/q2.cpp
@@ -5,13 +5,33 @@
 #include <iomanip>
 using namespace std;
 
-void sortArray();
+void sortAsc(int, int);
 
 int main()
 {
 	int arr[] = {-12, 3, -12, 4, 1, 1, -12, 1, -1, 1, 2, 3, 4, 2, 3, -12};
 	int arrSize = sizeof(arr)/sizeof(arr[0]);
-	cout << arrSize;
+	int p = 0, q = 0;
+	int newArr[p][q];
+	int count;
 
+	sortAsc(arr, arrSize);
+	cout << arr;
+	for (int i = 0; i < arrSize; i++) {
+		for (int j = 1; j < arrSize; j++) {
 
+		}
+	}
+}
+void sortAsc(int *arr, int n) {
+	int temp;
+	for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if ( arr[j] > arr[j+1]) {
+                temp = arr[j];
+   				arr[j] = arr[j+1];
+    			arr[j+1] = temp;
+			}
+		}
+    }
 }
